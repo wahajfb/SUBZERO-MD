426deac51521f9b7119bd223264d2a68dc65f51d
wajahat Jamil <wajahatj162@gmail.com>
2026-01-02 11:26:13 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~lvQ8HT", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "923701591721", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
