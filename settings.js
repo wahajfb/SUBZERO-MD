4ab71080b2df40e7e2b933d91f5c7eb3607c5c94
wajahat Jamil <wajahatj162@gmail.com>
2026-01-07 22:03:16 +0500

    Update settings.js for Ice-2GIew3

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "Ice~lvQ8HT", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "923701591721", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~2GIew3",
+  OWNER_NUMBER: "+923329340192", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
